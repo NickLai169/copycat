9aeec3a5abf5ff1fb982ab6e21f95037f815f1a0
Nick Lai <nickplai@berkeley.edu>
2020-08-12 00:05:11 +1200

    record function complete, beginning perform function

--- functions.py
+++ functions.py
@@ -16,18 +16,18 @@ recordings_path = directory + sep + "recordings"
 screen_dim = pyautogui.size()
 
 
-
 """
-Basic Recording function that records all the inputs and returns a new file
+Basic Recording function that records all the inputs and returns a new file.
+Doesn't allow files with duplucate names
 @param args: the input parameters on the command line in a list
 @return File which the record function has written to.
 """
 def record(args):
     recording_path = directory + sep + "recordings"
     now = time.localtime()
-    duration = 10
-    name = "{0}-{1}-{2} {3}:{4}:{5}".format(now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
-    retString = "Screen dimension: {0} | Date recorded: {1}".format(screen_dim, name)
+    duration = 3
+    name = "{0}-{1}-{2} {3}, {4}, {5}".format(now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+
     if len(args) == 1:
         try:
             duration = float(args[0])
@@ -41,9 +41,11 @@ def record(args):
             name = args[0]
             duration = float(args[1])
 
-    print("name: " + name + "({0})".format(type(name)))
-    print("duration: " + str(duration) + "({0})".format(type(duration)))
-    print(retString)
+    if os.path.exists(recording_path + sep + name + ".txt"):
+        print('Recording named "{0}" already exists. Please use alternative name or delete file'.format(name))
+        return
+
+    f = open(recordings_path + sep + name + ".txt", "a+")
 
     """
     Mouse position is formatted as: [time] mouse_pos [x] [y]
@@ -52,7 +54,7 @@ def record(args):
     """
     def on_move(x, y):
         # print("mouse is at position ({0}, {1})".format(x, y))
-        retString += "{2} mouse_pos {0} {1} \n".format(x/screen_dim, y/screen_dim, time.time())
+        f.write("{2} mouse_pos {0} {1} \n".format(x/screen_dim[0], y/screen_dim[1], time.time_ns()))
 
     """
     Mouse position is formatted as: [time] click_pos [x] [y] [button] [pressed]
@@ -64,7 +66,7 @@ def record(args):
     """
     def on_click(x, y, button, pressed):
         # print("Pressed: {0} | Button: {1} | position ({2}, {3})".format(pressed, button, x, y))
-        retString += "{4} click_pos {0} {1} {2} {3} \n".format(x/screen_dim, y/screen_dim, button, pressed, time.time())
+        f.write("{4} click_pos {0} {1} {2} {3} \n".format(x/screen_dim[0], y/screen_dim[1], button, pressed, time.time_ns()))
 
 
     """
@@ -76,27 +78,38 @@ def record(args):
     """
     def on_scroll(x, y, dx, dy):
         # print("Scrolling at ({0}, {1}) for dx: {2} and dy: {3}".format(x, y, dx, dy))
-        retString += "{0} scroll {1} {2} {3} {4} \n".format(time.time(), x/screen_dim, y/screen_dim, dx/screen_dim, dy/screen_dim)
+        f.write("{0} scroll {1} {2} {3} {4} \n".format(time.time_ns(), x/screen_dim[0], y/screen_dim[1], dx/screen_dim[0], dy/screen_dim[1]))
 
 
     """ Keyboard Listener below"""
 
+    """
+    Key presses are formatted as: [time] pressed [key]
+    @param key: The key object that was pressed
+    """
     def on_press(key):
         try:
-            print('alphanumeric key {0} pressed'.format(
-                key.char))
+            # print('alphanumeric key {0} pressed'.format(key.char))
+            f.write("{1} pressed {0} \n".format(key.char, time.time_ns()))
         except AttributeError:
-            print('special key {0} pressed'.format(
-                key))
+            # print('special key {0} pressed'.format(key))
+            f.write("{1} pressed {0} \n".format(key, time.time_ns()))
 
-
+    """
+    Key releases are formatted as: [time] pressed [key]
+    @param key: The key object that was pressed
+    """
     def on_release(key):
-        print('{0} released'.format(
-            key))
+        f.write('{0} release \n'.format(key))
         if key == keyboard.Key.esc:
+            print("Function terminated prematurely")
             return False
 
-    def time_me(t=3):
+    """
+    Timer function to terminate the listener after the indicated duration has passed
+    @param t: The duration that the listener should record for
+    """
+    def time_me(t):
         time.sleep(t)
         kl.stop()
         ml.stop()
@@ -107,9 +120,4 @@ def record(args):
             ml.join()
             kl.join()
 
-    print(retString)
-
-    # f = open(recordings_path + sep + "peepee.txt", "w+")
-    # f.write("Hell omy friends")
-    # f.write("Welcome to Game theory")
-    # f.close()
+    f.close()
